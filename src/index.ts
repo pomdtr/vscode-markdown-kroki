ef171e187f32eca7dd265a8066ca795134236f29
Joe Rocklin <joe.rocklin@gmail.com>
2024-01-17 11:43:53 -0500

    Fetch supported diagram types from server
    This will update the list of supported diagram types from the configured
    kroki server, defaulting to the hardcoded list if unable to fetch for
    some reason.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
 import pako from "pako";
 import { workspace } from "vscode";
 import * as vscode from 'vscode';
+import fetch from "node-fetch";
 
 export function activate() {
-  return {
-    extendMarkdownIt(md) {
+  let channel = vscode.window.createOutputChannel("markdown-kroki");
+
+  const config = workspace.getConfiguration("markdown-kroki");
+  let url = config.get<string>("url", "https://kroki.io");
+
+  let supportedDiagramTypes = staticSupportedDiagramTypes;
+
+  // Listen for configuration changes
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration("markdown-kroki")) {
       const config = workspace.getConfiguration("markdown-kroki");
 
+      // The setting has been changed, update it
+      let newUrl = config.get<string>("url", "https://kroki.io");
+      channel.appendLine(`URL updated: ${newUrl}`);
+
+      getSupportedDiagramTypes(newUrl, channel).then(function (data: string[]) {
+        supportedDiagramTypes = data;
+      });
+    }
+  });
+
+  getSupportedDiagramTypes(url, channel).then(function (data: string[]) {
+    supportedDiagramTypes = data;
+  });
+
+  return {
+    extendMarkdownIt(md) {
       const fence = md.renderer.rules.fence.bind(md.renderer.rules);
       const diagramPrefix = config.get<string>("prefix", "").toLowerCase();
-      const url = config.get<string>("url", "https://kroki.io");
-    
+
       md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-        const token = tokens[idx]
+        const token = tokens[idx];
 
         const diagramType = (() => {
           var tokenName = token.info.toLowerCase();
@@ -24,14 +48,48 @@ export function activate() {
         })();
 
         if (supportedDiagramTypes.includes(diagramType)) {
+          channel.appendLine("Rendering " + diagramType);
+
           const encodedDiagram = encodeDiagram(token.content.trim());
           return `<p><img src="${url}/${diagramType}/svg/${encodedDiagram}"></p>`;
+        } else {
+          channel.appendLine(
+            "Skipping fence " + diagramType + ". Unknown type"
+          );
         }
         return fence(tokens, idx, options, env, slf);
       };
       return md;
     },
   };
+}
+
+async function getSupportedDiagramTypes(
+  url: string,
+  channel: vscode.OutputChannel
+): Promise<string[]> {
+  let supportedDiagramTypes = staticSupportedDiagramTypes;
+
+  let data;
+  const healthURL = `${url}/v1/health`;
+  channel.appendLine("Fetching known types from " + healthURL);
+
+  const response = await fetch(healthURL);
+  if (!response.ok) {
+    channel.appendLine(
+      "Error fetching supported document types, will use hardcoded list"
+    );
+    channel.appendLine("HTTP Error: " + response.status);
+    return;
+  }
+
+  data = await response.json();
+
+  supportedDiagramTypes = Object.keys(data.version).filter(
+    (key) => key !== "kroki"
+  );
+  channel.appendLine("Setting supported types to: " + supportedDiagramTypes);
+  return supportedDiagramTypes;
 }
 
 function encodeDiagram(source) {
@@ -43,7 +101,7 @@ function encodeDiagram(source) {
     .replace(/\//g, "_");
 }
 
-const supportedDiagramTypes = [
+const staticSupportedDiagramTypes = [
   "actdiag",
   "blockdiag",
   "bpmn",
